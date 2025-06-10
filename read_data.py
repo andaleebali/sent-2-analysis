7ed8beb7bbeb93b40f2cc6d8b0080d9386ad4c84
Andaleeb Ali <s1885898@ed.ac.uk>
2025-06-10 23:17:41 +0100

    calculates ndvi from sentinel 2 data

---
+++ read_data.py
@@ -0,0 +1,23 @@
+import rasterio
+import numpy
+import matplotlib.pyplot as plt
+
+redfile = 'Image\S2B_MSIL2A_20250531T220619_N0511_R086_T60HWC_20250531T233234.SAFE\S2B_MSIL2A_20250531T220619_N0511_R086_T60HWC_20250531T233234.SAFE\GRANULE\L2A_T60HWC_A043012_20250531T220639\IMG_DATA\R10m\T60HWC_20250531T220619_B04_10m.jp2'
+
+nirfile = 'Image\S2B_MSIL2A_20250531T220619_N0511_R086_T60HWC_20250531T233234.SAFE\S2B_MSIL2A_20250531T220619_N0511_R086_T60HWC_20250531T233234.SAFE\GRANULE\L2A_T60HWC_A043012_20250531T220639\IMG_DATA\R10m\T60HWC_20250531T220619_B08_10m.jp2'
+
+
+with rasterio.open(redfile) as red:
+    red = red.read(1)
+
+with rasterio.open(nirfile) as nir:
+    nir = nir.read(1)
+
+ndvi = (nir.astype(float) - red.astype(float))/(nir.astype(float) + red.astype(float))
+
+print(ndvi)
+
+plt.imshow(ndvi, cmap='BuGn')
+plt.colorbar(label='NDVI')
+plt.title('NDVI from Sentinel-2')
+plt.show()
