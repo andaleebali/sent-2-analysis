46a2884d3d0e222bdb2fcff71e80e4a1527e5022
Andaleeb Ali <s1885898@ed.ac.uk>
2025-07-06 16:05:56 +0100

    update structure

--- ndvi_calculator.py
+++ ndvi_calculator.py
@@ -1,23 +1,26 @@
 import rasterio
-import numpy
 import matplotlib.pyplot as plt
+from pathlib import Path
 
-redfile = 'Image\S2B_MSIL2A_20250531T220619_N0511_R086_T60HWC_20250531T233234.SAFE\S2B_MSIL2A_20250531T220619_N0511_R086_T60HWC_20250531T233234.SAFE\GRANULE\L2A_T60HWC_A043012_20250531T220639\IMG_DATA\R10m\T60HWC_20250531T220619_B04_10m.jp2'
+def main():
+    redfile = Path('Image\S2B_MSIL2A_20250531T220619_N0511_R086_T60HWC_20250531T233234.SAFE\S2B_MSIL2A_20250531T220619_N0511_R086_T60HWC_20250531T233234.SAFE\GRANULE\L2A_T60HWC_A043012_20250531T220639\IMG_DATA\R10m\T60HWC_20250531T220619_B04_10m.jp2')
 
-nirfile = 'Image\S2B_MSIL2A_20250531T220619_N0511_R086_T60HWC_20250531T233234.SAFE\S2B_MSIL2A_20250531T220619_N0511_R086_T60HWC_20250531T233234.SAFE\GRANULE\L2A_T60HWC_A043012_20250531T220639\IMG_DATA\R10m\T60HWC_20250531T220619_B08_10m.jp2'
+    nirfile = Path('Image\S2B_MSIL2A_20250531T220619_N0511_R086_T60HWC_20250531T233234.SAFE\S2B_MSIL2A_20250531T220619_N0511_R086_T60HWC_20250531T233234.SAFE\GRANULE\L2A_T60HWC_A043012_20250531T220639\IMG_DATA\R10m\T60HWC_20250531T220619_B08_10m.jp2')
+    
+    with rasterio.open(redfile) as red:
+        red = red.read(1)
 
+    with rasterio.open(nirfile) as nir:
+        nir = nir.read(1)
 
-with rasterio.open(redfile) as red:
-    red = red.read(1)
+    ndvi = (nir.astype(float) - red.astype(float))/(nir.astype(float) + red.astype(float))
 
-with rasterio.open(nirfile) as nir:
-    nir = nir.read(1)
+    print(ndvi)
 
-ndvi = (nir.astype(float) - red.astype(float))/(nir.astype(float) + red.astype(float))
+    plt.imshow(ndvi, cmap='BuGn')
+    plt.colorbar(label='NDVI')
+    plt.title('NDVI from Sentinel-2')
+    plt.show()
 
-print(ndvi)
-
-plt.imshow(ndvi, cmap='BuGn')
-plt.colorbar(label='NDVI')
-plt.title('NDVI from Sentinel-2')
-plt.show()
+if __name__=="__main__":
+    main()
